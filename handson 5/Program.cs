993ed6c9bf1dcb344d6d13fb7a2f69d6e2b15f2b
PR20276046 <PR20276046@APL-157L3771355>
2021-10-13 08:19:02 +0530

    Add project files.

---
+++ handson 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using System;
+public class Demo
+{
+   public static void Main()
+    {
+        string myStr; 
+        string rev;
+        string res2;
+        Console.WriteLine("enter the string");
+        myStr = Console.ReadLine();
+        rev = "";
+        
+        string myStr2 = String.Copy(myStr);// copy the string to another variable
+        res2 = myStr2.Substring(2);
+        string res = myStr.Substring(1);//prints the char of string after the second position
+        //now the replacing of the string starts from here
+        string op = string.Empty;
+        Regex rgx = new Regex("[^A-Za-z0-9]");
+        for (int i = 0; i < myStr.Length; i++)
+        {
+            op += (rgx.IsMatch(myStr[i].ToString())) ? "$" : myStr[i].ToString();
+        }
+        // replacing of the string ends here
+        // find string length
+        int len;
+        len = myStr.Length - 1;
+        while (len >= 0)
+        {
+            rev = rev + myStr[len];
+            len--;
+        }
+
+        Console.WriteLine("Reversed String is {0}", rev);
+        Console.WriteLine("From the 2nd position is {0} ",res);
+        Console.WriteLine("after the replacement of string by $ is {0} ",op);
+        Console.WriteLine("After the modification in the position string is {0}", res2);
+        Console.ReadLine();
+    }
+}
